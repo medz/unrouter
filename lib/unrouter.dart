556af7dc37bc608ec8864302e2aee50cb3067c76
Seven Du <5564821+medz@users.noreply.github.com>
2025-12-18 22:30:22 +0800

    Export additional router components and reorder exports

--- lib/unrouter.dart
+++ lib/unrouter.dart
@@ -1,8 +1,13 @@
 export 'src/history/history.dart';
 export 'src/history/memory.dart';
 
-export 'src/router.dart';
+export 'src/widgets/outlet.dart';
+
 export 'src/inlet.dart';
+export 'src/route_information_parser.dart';
+export 'src/route_information_provider.dart';
+export 'src/route_matcher.dart' show MatchedRoute;
+export 'src/router.dart';
+export 'src/router_delegate.dart';
 export 'src/router_state.dart';
 export 'src/url_strategy.dart';
-export 'src/widgets/outlet.dart';
