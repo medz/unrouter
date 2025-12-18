f944dacc24c81d9ed9e20aeaba18630f34824c73
Seven Du <5564821+medz@users.noreply.github.com>
2025-12-18 16:18:20 +0800

    Add browser and hash history implementations

--- lib/unrouter.dart
+++ lib/unrouter.dart
@@ -2,7 +2,7 @@ export 'src/router.dart';
 export 'src/inlet.dart';
 export 'src/router_state.dart';
 
-export 'src/history/types.dart';
+export 'src/history/history.dart';
 export 'src/history/memory.dart';
 
 export 'src/widgets/outlet.dart';
