e3d765a29dbe47d802a1bed573abb1e78224b22e
Seven Du <5564821+medz@users.noreply.github.com>
2025-12-19 00:57:01 +0800

    Remove internal route information parser and provider classes
    Move the classes into the main router file as private implementations

--- lib/src/router.dart
+++ lib/src/router.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/widgets.dart';
 
-import '_internal/route_information_parser.dart';
-import '_internal/route_information_provider.dart';
+import '_internal/create_history.memory.dart'
+    if (dart.library.js_interop) '_internal/create_history.browser.dart';
 import 'history/history.dart';
 import 'inlet.dart';
 import 'router_delegate.dart';
 import 'url_strategy.dart';
-
-import '_internal/create_history.memory.dart'
-    if (dart.library.js_interop) '_internal/create_history.browser.dart';
 
 class Unrouter extends StatelessWidget
     implements RouterConfig<RouteInformation> {
@@ -29,17 +26,17 @@ class Unrouter extends StatelessWidget
 
   @override
   late final RouteInformationProvider routeInformationProvider =
-      UnrouterInformationProvider(history);
+      _InformationProvider(history);
 
   @override
   late final UnrouterDelegate routerDelegate = UnrouterDelegate(
-    routes: routes,
     history: history,
+    routes: routes,
   );
 
   @override
   RouteInformationParser<RouteInformation> get routeInformationParser =>
-      const UnrouterInformationParser();
+      const _InformationParser();
 
   void navigate(Uri uri, {Object? state, bool replace = false}) {
     final resolvedUri = routerDelegate.resolveUri(uri);
@@ -61,5 +58,64 @@ class Unrouter extends StatelessWidget
   @protected
   Widget build(BuildContext context) {
     return Router.withConfig(config: this);
+  }
+}
+
+class _InformationProvider extends RouteInformationProvider
+    with ChangeNotifier {
+  _InformationProvider(this.history) : value = history.location {
+    unlisten = history.listen((event) {
+      value = event.location;
+      notifyListeners();
+    });
+  }
+
+  final History history;
+  late final void Function() unlisten;
+
+  @override
+  RouteInformation value;
+
+  @override
+  void routerReportsNewRouteInformation(
+    RouteInformation routeInformation, {
+    RouteInformationReportingType type = RouteInformationReportingType.none,
+  }) {
+    final newUri = routeInformation.uri;
+    final currentUri = history.location.uri;
+
+    if (newUri != currentUri) {
+      switch (type) {
+        case RouteInformationReportingType.none:
+        case RouteInformationReportingType.neglect:
+          // Don't update history
+          break;
+        case RouteInformationReportingType.navigate:
+          history.push(newUri, routeInformation.state);
+          break;
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    unlisten();
+    super.dispose();
+  }
+}
+
+class _InformationParser extends RouteInformationParser<RouteInformation> {
+  const _InformationParser();
+
+  @override
+  Future<RouteInformation> parseRouteInformation(
+    RouteInformation routeInformation,
+  ) async {
+    return routeInformation;
+  }
+
+  @override
+  RouteInformation? restoreRouteInformation(RouteInformation configuration) {
+    return configuration;
   }
 }
